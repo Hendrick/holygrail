ad44396ad07e8af43366e2ce28e2ed0c0a16378c
Larry Staton Jr <larry@larrystaton.com>
2015-11-05 05:36:24 +0000

    Add timbre config
    Only using `spit-appender` for now

--- src/clj/holy_grail/core.clj
+++ src/clj/holy_grail/core.clj
@@ -1,12 +1,16 @@
 (ns holy-grail.core
   (:gen-class)
   (:require [holy-grail.systems :refer [prod-system]]
-            [reloaded.repl :refer [system init start stop go reset]]))
+            [reloaded.repl :refer [system init start stop go reset]]
+            [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.core :as appenders]))
 
 (defn -main
   "Start a production system, unless a system is passed as argument (as in the dev-run task)."
   [& args]
   (let [system (or (first args) #'prod-system)]
+    (timbre/merge-config! {:appenders {:immutant-dot-log
+                                       (appenders/spit-appender {:fname
+                                                                 "/var/log/immutant/immutant.log"})}})
     (reloaded.repl/set-init! system)
     (go)))
-
